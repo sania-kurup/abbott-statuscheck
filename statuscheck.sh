fa36900ca0a6e47be81e47ad8371290300698787
sania-kurup <90313230+sania-kurup@users.noreply.github.com>
2022-01-12 16:36:31 +0530

    Update statuscheck.sh

--- statuscheck.sh
+++ statuscheck.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-while read LINE; do
-  curl -o /dev/null --silent --get --write-out "%{http_code} $LINE\n" "$LINE"
-done < all-urls.txt
+while read LINE; do   curl -o /dev/null --silent --get --write-out "%{http_code} $LINE\n" "$LINE"; done < all-urls.txt | grep 'en' > file1.txt 
